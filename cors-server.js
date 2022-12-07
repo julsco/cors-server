3cf6a9b38edb394bdc0e87111dc4dc3db559c0d9
julsco <juliancscotti@gmail.com>
2022-12-07 20:40:28 +0000

    cors

--- cors-server.js
+++ cors-server.js
@@ -1,14 +1,41 @@
-const cors_proxy = require('cors-anywhere');
+var express = require('express'),
+    request = require('request'),
+    bodyParser = require('body-parser'),
+    app = express();
 
-const host = process.env.HOST || '0.0.0.0';
+var myLimit = typeof(process.argv[2]) != 'undefined' ? process.argv[2] : '100kb';
+console.log('Using limit: ', myLimit);
 
-// Listen on a specific port via the PORT environment variable
-const port = process.env.PORT || 8080;
+app.use(bodyParser.json({limit: myLimit}));
 
-cors_proxy
-  .createServer({
-    originWhitelist: [], // Allow all origins
-  })
-  .listen(port, host, function () {
-    console.log('Running CORS Anywhere on ' + host + ':' + port);
-  });
+app.all('*', function (req, res, next) {
+
+    // Set CORS headers: allow all origins, methods, and headers: you may want to lock this down in a production environment
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET, PUT, PATCH, POST, DELETE");
+    res.header("Access-Control-Allow-Headers", req.header('access-control-request-headers'));
+
+    if (req.method === 'OPTIONS') {
+        // CORS Preflight
+        res.send();
+    } else {
+        var targetURL = req.header('Target-URL');
+        if (!targetURL) {
+            res.send(500, { error: 'There is no Target-Endpoint header in the request' });
+            return;
+        }
+        request({ url: targetURL + req.url, method: req.method, json: req.body, headers: {'Authorization': req.header('Authorization')} },
+            function (error, response, body) {
+                if (error) {
+                    console.error('error: ' + response.statusCode)
+                }
+//                console.log(body);
+            }).pipe(res);
+    }
+});
+
+app.set('port', process.env.PORT || 3000);
+
+app.listen(app.get('port'), function () {
+    console.log('Proxy server listening on port ' + app.get('port'));
+});
